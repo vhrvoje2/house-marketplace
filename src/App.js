638f9bf9f7641d98f83b58905aab513b8d742944
vhrvoje2 <vhrvoje2@yahoo.com>
2022-03-26 19:17:34 +0100

    Initial Firebase setup, assets, navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Explore from "../src/pages/Explore";
+import Offers from "../src/pages/Offers";
+import Profile from "../src/pages/Profile";
+import Signin from "../src/pages/Signin";
+import Signup from "../src/pages/Signup";
+import ForgotPassword from "../src/pages/ForgotPassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+function ForgotPassword() {
+    return (
+        <>
+            <h1>ForgotPassword</h1>
+        </>
+    )
+}
+
+export default ForgotPassword;
